087c70ac40c25ccfde41de146ad6eb5c0b805dc7
ahmedmoselhi <ahmedmoselhi55@gmail.com>
2026-04-27 01:29:05 +0300

    Fix MenuList constructor call in ServiceList (#2)

--- Lamedb.py
+++ Lamedb.py
@@ -37,27 +37,46 @@ class Lamedb:
 		puffer = []
 		puffer.append("eDVB services /4/\n")
 		puffer.append("transponders\n")
-		for tp in self.database:
-			tp = self.database[tp]
-			puffer.append(("%s:%s:%s\n")%(tp["namespace"],tp["tsid"],tp["onid"]))
-			if tp["namespace"][:4].lower()=="ffff":
-				puffer.append(("\tc %s:%s:%s:%s:%s:%s\n")%(tp["frequency"],tp["symbol_rate"],tp["inversion"],tp["modulation"],tp["fec_inner"],tp["flags"]))
-			elif tp["namespace"][:4].lower()=="eeee":
-				puffer.append(("\tt %s:%s:%s:%s:%s:%s:%s:%s:%s:%s\n")%(tp["frequency"],tp["bandwidth"],tp["code_rate_HP"],tp["code_rate_LP"],tp["modulation"],tp["transmission_mode"],tp["guard_interval"],tp["hierarchy"],tp["inversion"],tp["flags"]))
+		for tp_key in self.database:
+			tp = self.database[tp_key]
+			puffer.append(("%s:%s:%s\n")%(tp.get("namespace", "0"), tp.get("tsid", "0"), tp.get("onid", "0")))
+			if tp.get("namespace", "0")[:4].lower()=="ffff":
+				c_params = [
+					tp.get("frequency", "0"), tp.get("symbol_rate", "0"), tp.get("inversion", "0"),
+					tp.get("modulation", "0"), tp.get("fec_inner", "0"), tp.get("flags", "0")
+				]
+				c_params.extend(tp.get("extra_cable_params", []))
+				puffer.append(("\tc %s\n") % ":".join(c_params))
+			elif tp.get("namespace", "0")[:4].lower()=="eeee":
+				t_params = [
+					tp.get("frequency", "0"), tp.get("bandwidth", "0"), tp.get("code_rate_HP", "0"),
+					tp.get("code_rate_LP", "0"), tp.get("modulation", "0"), tp.get("transmission_mode", "0"),
+					tp.get("guard_interval", "0"), tp.get("hierarchy", "0"), tp.get("inversion", "0"), tp.get("flags", "0")
+				]
+				t_params.extend(tp.get("extra_terr_params", []))
+				puffer.append(("\tt %s\n") % ":".join(t_params))
 			else:
-				sys = tp.get("system",None)
-				if sys is None or sys == "0":
-					puffer.append(("\ts %s:%s:%s:%s:%s:%s:%s\n")%(tp["frequency"],tp["symbol_rate"],tp["polarization"],tp["fec_inner"],tp["position"],tp["inversion"],tp["flags"]))
-				else:
-					puffer.append(("\ts %s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s\n")%(tp["frequency"],tp["symbol_rate"],tp["polarization"],tp["fec_inner"],tp["position"],tp["inversion"],tp["flags"],tp["system"],tp["modulation"],tp["rolloff"],tp["pilot"]))
+				s_params = [
+					tp.get("frequency", "0"), tp.get("symbol_rate", "0"), tp.get("polarization", "0"), tp.get("fec_inner", "0"),
+					tp.get("position", "0"), tp.get("inversion", "0"), tp.get("flags", "0")
+				]
+				if "system" in tp:
+					s_params.extend([
+						tp.get("system", "0"),
+						tp.get("modulation", "1"),
+						tp.get("rolloff", "0"),
+						tp.get("pilot", "0"),
+					])
+				s_params.extend(tp.get("extra_sat_params", []))
+				puffer.append(("\ts %s\n") % ":".join(s_params))
 			puffer.append("/\n")	
 		puffer.append("end\n")
 		puffer.append("services\n")
-		for tp in self.database:
-			for service in self.database[tp]["services"]:
-				service = self.database[tp]["services"][service]
-				puffer.append(("%s:%s:%s:%s:%s:%s\n")%(service["sid"],service["namespace"],service["tsid"],service["onid"],service["type"],service["number"]))
-				puffer.append(("%s\n")%service["name"])
+		for tp_key in self.database:
+			for service_key in self.database[tp_key].get("services", {}):
+				service = self.database[tp_key]["services"][service_key]
+				puffer.append(("%s:%s:%s:%s:%s:%s\n")%(service.get("sid", "0"), service.get("namespace", "0"), service.get("tsid", "0"), service.get("onid", "0"), service.get("type", "0"), service.get("number", "0")))
+				puffer.append(("%s\n")%service.get("name", ""))
 				tmp = ""
 				cacheIDs = service.get("cacheIDs",None)
 				if cacheIDs is not None:
@@ -70,9 +89,13 @@ class Lamedb:
 				flags = service.get("flags",None)
 				if flags is not None and int(flags,16)!=0:
 					tmp += ",f:" + flags
-				puffer.append(("p:%s%s\n")%(service["provider"],tmp))
+				extras = service.get("extras", None)
+				if extras is not None:
+					for extra in extras:
+						tmp += "," + extra
+				puffer.append(("p:%s%s\n")%(service.get("provider", ""),tmp))
 		puffer.append("end\n")
-		puffer.append("Have a lot of girls\n")
+		puffer.append("Have a lot of bugs!\n")
 		f = open("/etc/enigma2/lamedb","w")
 		f.writelines(puffer)
 		f.close()
@@ -80,32 +103,30 @@ class Lamedb:
 	def getServices(self, lamedb):
 		print("getServices", end=' ')
 		if lamedb is None:
-			return
+			return []
 		collect = False
-		state = 0
 		services = []
-		for x in range(len(lamedb)-2):
-			if lamedb[x] == "services\n":
+		x = 0
+		while x < len(lamedb):
+			line = lamedb[x]
+			if line == "services\n":
 				collect = True
+				x += 1
 				continue
-			if lamedb[x] == "end\n":
+			if line == "end\n":
 				collect = False
+				x += 1
 				continue
-			if collect:
-				tmp = lamedb[x].split(":")
-				if len(tmp) >= 4:
-					if tmp[1]+tmp[2]+tmp[3] in self.database:
-						tmp = lamedb[x+2].split(",")
-						for key in tmp:
-							key = key.split(":")
-#							if len(key)==2 and key[0].strip() in ("c","C","f","p",):
-							if len(key)==2:
-								continue
-							else:
-								break
-						else:
-							services.append((lamedb[x],lamedb[x+1],lamedb[x+2],))
-#							self.translateService((lamedb[x],lamedb[x+1],lamedb[x+2],))
+			
+			if collect and (x + 2) < len(lamedb):
+				tmp = line.split(":")
+				if len(tmp) >= 6:
+					transponder_key = (tmp[1] + tmp[2] + tmp[3]).lower()
+					if transponder_key in self.database:
+						services.append((lamedb[x], lamedb[x+1], lamedb[x+2]))
+				x += 3
+			else:
+				x += 1
 		print(" fertig")
 		return services
 	
@@ -116,67 +137,67 @@ class Lamedb:
 			return
 		service = {}
 		tp_data = serviceData[0].strip().split(":")
-		if len(tp_data) > len(t1):
-			print("falsche Anzahl Parameter (6 erwartet) in ",serviceData[0])
-			return
-		for y in range(len(t1)):
+		for y in range(min(len(t1), len(tp_data))):
 			service.update({t1[y]:tp_data[y]})
-		name = serviceData[1].strip().replace('\xc2\x86', '').replace('\xc2\x87', '')
+		name = serviceData[1].strip('\n').replace('\xc2\x86', '').replace('\xc2\x87', '')
 		service.update({"name":name})
 		provider_data = serviceData[2].strip().split(",")
 		for y in provider_data:
-			raw = y.split(":")
-#			if len(raw)==2 and raw[0].strip() in ("c","C","f","p",):
+			raw = y.split(":", 1)
+			if len(raw) != 2:
+				continue
 			if raw[0]=="p":
-				service["provider"] = raw[1].strip().replace('\xc2\x86', '').replace('\xc2\x87', '')
+				service["provider"] = raw[1].replace('\xc2\x86', '').replace('\xc2\x87', '')
 			elif raw[0]=="c":
 				cacheIDs = service.get("cacheIDs",None)
 				if cacheIDs is None:
-					service["cacheIDs"] = [raw[1].strip(),]
+					service["cacheIDs"] = [raw[1],]
 				else:
-					cacheIDs.append(raw[1].strip())
+					cacheIDs.append(raw[1])
 			elif raw[0]=="C":
 				caIDs = service.get("caIDs",None)
 				if caIDs is None:
-					service["caIDs"] = [raw[1].strip(),]
+					service["caIDs"] = [raw[1],]
 				else:
-					caIDs.append(raw[1].strip())
+					caIDs.append(raw[1])
 			elif raw[0]=="f":
-				service["flags"] = raw[1].strip()
+				service["flags"] = raw[1]
 			else:
-				print("unbekanter Parameter:",raw[0])
-				print("in:",y)
-#			else:
-#				print "hmm, da stimmt was mit den Daten nicht:",raw
-#				break
+				extras = service.get("extras", [])
+				extras.append(y)
+				service["extras"] = extras
+				
+		if "namespace" not in service or "tsid" not in service or "onid" not in service or "sid" not in service:
+			return
+			
+		uniqueTransponder = (service["namespace"] + service["tsid"] + service["onid"]).lower()
+		if uniqueTransponder not in self.database:
+			return
+			
+		if (int(service.get("flags","0"),16) & dxNoDVB):
+			tmp = ''
+			for cacheID in service.get("cacheIDs",[]):
+				tmp += cacheID
+			uniqueService = uniqueTransponder + tmp
 		else:
-			uniqueTransponder = service["namespace"]+service["tsid"]+service["onid"]
-			if (int(service.get("flags","0"),16) & dxNoDVB):
-				tmp = ''
-				for cacheID in service.get("cacheIDs",[]):
-					tmp += cacheID
-				uniqueService = uniqueTransponder + tmp
-			else:
-				uniqueService = uniqueTransponder + service["sid"]
-			service["usk"] = uniqueService
-			self.database[uniqueTransponder]["services"][uniqueService] = service
-			self.readcnt += 1
-			self.databaseState=3
+			uniqueService = uniqueTransponder + service["sid"]
+		service["usk"] = uniqueService
+		self.database[uniqueTransponder]["services"][uniqueService] = service
+		self.readcnt += 1
+		self.databaseState=3
 	
 	def translateServices(self, services):
 		if services is None:
 			return
 		for x in services:
 			self.translateService(x)
-		else:
-			self.databaseState=4
+		self.databaseState=4
 	
 
 	def getTransponders(self, lamedb):
 		if lamedb is None:
-			return
+			return []
 		collect = False
-		state = 0
 		transponders = []
 		tp = []
 		for x in lamedb:
@@ -245,56 +266,56 @@ class Lamedb:
 			if len(x[0]) > len(t1):
 				print("zu viele Parameter (t1) in ",x[0])
 				continue
+			if len(x) < 2 or not x[1]:
+				continue
 			freq = x[1][0].split()
 			if len(freq) != 2:
 				print("zwei Parameter erwartet in ",freq)
 				continue
-			tp = {"services":[]}
+			tp = {"services":{}}
 			x[1][0] = freq[1]
 			if freq[0] == "s" or freq[0] == "S":
-				if ((self.version == 3) and len(x[1]) > len(t2_sv3)) or ((self.version == 4) and len(x[1]) > len(t2_sv4)):
-					print("zu viele Parameter (t2) in ",x[1])
-					continue
 				for y in range(len(x[0])):
 					tp.update({t1[y]:x[0][y]})
-				for y in range(len(x[1])):
-					if self.version == 3:	
+				for y in range(min(len(x[1]), len(t2_sv4 if self.version == 4 else t2_sv3))):
+					if self.version == 3:
 						tp.update({t2_sv3[y]:x[1][y]})
 					elif self.version == 4:
 						tp.update({t2_sv4[y]:x[1][y]})
-				pos = int(tp.get("namespace"),16) >>16
-				if pos > 1799:
-					pos -= 3600
-				if pos != int(tp.get("position")):
-					print("Namespace %s und Position %s sind  nicht identisch"% (tp.get("namespace"), tp.get("position")))
-					continue
-				self.database[tp["namespace"]+tp["tsid"]+tp["onid"]] = tp
-				self.database[tp["namespace"]+tp["tsid"]+tp["onid"]]["services"] = {}
-				self.databaseState=1
+				if self.version == 4 and len(x[1]) > len(t2_sv4):
+					tp["extra_sat_params"] = x[1][len(t2_sv4):]
+					
+				# Removed strict position check since e2 permits mismatches
+				
+				transponder_key = (tp.get("namespace", "") + tp.get("tsid", "") + tp.get("onid", "")).lower()
+				if transponder_key:
+					self.database[transponder_key] = tp
+					self.databaseState=1
 			elif freq[0] == "c" or freq[0] == "C":
-				if len(x[1]) > len(t2_c):
-					print("zu viele Parameter (t2) in ",x[1])
-					continue
 				for y in range(len(x[0])):
 					tp.update({t1[y]:x[0][y]})
-				for y in range(len(x[1])):
+				for y in range(min(len(x[1]), len(t2_c))):
 					tp.update({t2_c[y]:x[1][y]})
-				self.database[tp["namespace"]+tp["tsid"]+tp["onid"]] = tp
-				self.database[tp["namespace"]+tp["tsid"]+tp["onid"]]["services"] = {}
-				self.databaseState=1
+				if len(x[1]) > len(t2_c):
+					tp["extra_cable_params"] = x[1][len(t2_c):]
+					
+				transponder_key = (tp.get("namespace", "") + tp.get("tsid", "") + tp.get("onid", "")).lower()
+				if transponder_key:
+					self.database[transponder_key] = tp
+					self.databaseState=1
 			elif freq[0] == "t" or freq[0] == "T":
-				if len(x[1]) > len(t2_t):
-					print("zu viele Parameter (t2) in ",x[1])
-					continue
 				for y in range(len(x[0])):
 					tp.update({t1[y]:x[0][y]})
-				for y in range(len(x[1])):
+				for y in range(min(len(x[1]), len(t2_t))):
 					tp.update({t2_t[y]:x[1][y]})
-				self.database[tp["namespace"]+tp["tsid"]+tp["onid"]] = tp
-				self.database[tp["namespace"]+tp["tsid"]+tp["onid"]]["services"] = {}
-				self.databaseState=1
-		else:
-			self.databaseState=2
+				if len(x[1]) > len(t2_t):
+					tp["extra_terr_params"] = x[1][len(t2_t):]
+					
+				transponder_key = (tp.get("namespace", "") + tp.get("tsid", "") + tp.get("onid", "")).lower()
+				if transponder_key:
+					self.database[transponder_key] = tp
+					self.databaseState=1
+		self.databaseState=2
 
 dxNoSDT=1    	# don't get SDT
 dxDontshow=2
